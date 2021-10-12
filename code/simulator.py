7815ccd5ff2506b4effce30d1f4bdf5e62c34e08
ben <ben.work.smith@gmail.com>
2021-10-12 10:51:16 +0100

    Add user input to test each function

--- code/simulator.py
+++ code/simulator.py
@@ -37,23 +37,16 @@ class Pod:
 # List of all the pods if a new one is to be added it can be done here
 pods = [Pod('Living Quarters',['outside','empty','Connecting Corridor','empty'],['airlock','empty','normal','empty'],[]),Pod('Connecting Corridor',['Living Quarters','Food Production','Engineering Workshop/Mining Operations/Storage','Life Support/Power Plant/Recycling'],['normal','normal','normal','normal'],['Comms And Control Centre']),Pod('Comms And Control Centre',['Connecting Corridor','Connecting Corridor'],['normal','normal'],[]),Pod('Engineering Workshop/Mining Operations/Storage',['Connecting Corridor','Bio-Research','outside','empty'],['normal','airlock','airlock','empty'],[]),Pod('Bio-Research',['outside','Engineering Workshop/Mining Operations/Storage'],['empty','airlock'],[]),Pod('Food Production',['empty','Connecting Corridor'],['empty','normal'],[]),Pod('Life Support/Power Plant/Recycling',['Connecting Corridor','outside'],['normal','airlock'],[]),Pod('Storage (External)',['outside','empty'],['airlock','empty'],[]),Pod('Emergency Quarters',['empty','empty','empty','outside'],['empty','empty','empty','airlock'],[])]
 
-# Displaying all pods and there attributes to the screen
-[print(pod) for pod in pods]
-
 # Finds the index of a certain pod based on it's name
 def index(source):
     for i in range(0,len(pods)):
         if pods[i].name == source:
             return i
 
-# Source and target locations for path finder
-source = 'Living Quarters'
-target = 'Bio-Research'
-
 # Function to find the path from a source location to a target location
-def findpath(source, target, newplace, path, recursive):
+def findpath(source, target, newplace, path):
     path = path
-    if not recursive:
+    if not newplace:
         # Moves source location to start of array so that is the pod we are starting from
         pods.insert(0, pods.pop(index(source)))
     i = newplace
@@ -68,20 +61,33 @@ def findpath(source, target, newplace, path, recursive):
             # If the door has not been searched yet and it is not leading outside or empty it will re run the function to check all of that pods doors for the target
             if door not in ['empty','outside'] and door not in path:
                 newplace = index(door)
-                result =  findpath(source, target, newplace, path, 1)
+                result =  findpath(source, target, newplace, path)
                 if result == None:
                     del path[-1]
                 else: return result
-
-# Takes the path as a variable
-finalpath = findpath(source, target,0,[],0)
-if finalpath != None:
-    print(f'Source: {source}\nTarget: {target}\n{finalpath}')
-else:
-    print("Must go Outside")
 
 # Locks all doors in a given pod
 def lockdown(pod):
     [print(f'\nLocking {door} door\n') if door != 'empty' else '' for door in pods[index(pod)].door_type]
 
-lockdown('Connecting Corridor')
+while True:
+    check = int(input('\n1 - Find Path\n2 - Lockdown Pod\n3 - Show all pods\n4 - Exit\n>'))
+    if check == 1:
+        # Source and target locations for path finder
+        #source = 'Living Quarters'
+        #target = 'Bio-Research'
+        source = input('Enter Starting Location\n>')
+        target = input('Enter target location\n>')
+        # Takes the path as a variable
+        finalpath = findpath(source, target,0,[])
+        if finalpath != None:
+            print(f'Source: {source}\nTarget: {target}\n{finalpath}')
+        else:
+            print("Must go Outside")
+    elif check == 2:
+        lockdown(input('Enter pod to lockdown\n>'))
+    elif check == 3:
+        # Displaying all pods and there attributes to the screen
+        [print(pod) for pod in pods]
+    elif check == 4:
+        break
