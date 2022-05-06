d5c72f2abf59fe7614cf2fe9130df820625d4181
Gabriel Smith <44162095+RaveonUK@users.noreply.github.com>
2022-05-06 15:03:46 +0100

    Update scenario_gui.py

--- code/scenario_gui.py
+++ code/scenario_gui.py
@@ -108,7 +108,7 @@ class MainWindow:
         input_frame = init_input_frame(self.canvas, input_text)
         
         input_frame.place(relx=0.5, rely=0.7, relwidth=0.95, relheight=0.075, anchor="center")
-        pod_name_arr = ["Living Quarters", "Connecting Corridor", "Emergency Quarters", "Life Support/Power Plant/Recycling", "Food Production", "Engineering Workshop/Mining Operations/Storage", "Bio-Research", "Storage (External)", "Comms And Control Centre"]
+        pod_name_arr = ["Living Quarters", "Connecting Corridor", "Emergency Quarters", "Life Support/Power Plant/Recycling", "Food Production", "Engineering Workshop/Mining Operations/Storage", "Bio-Research", "Storage (External)", "Comms And Control Centre", "Airlock1", "Airlock2", "Airlock3", "Airlock4", "Airlock5", "Airlock6"]
         newline = '\n'
         output = list([f'{index+1} - {pod}' for index, pod in enumerate(pod_name_arr)])
         instructions = Label(self.canvas,text=f"Please enter the pod the disaster should take place in{newline}{newline.join(output)}",font=20, bg='grey')
